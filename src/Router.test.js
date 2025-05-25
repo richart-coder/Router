ccbf72070816dee2d79ce7e8ae190c1f27710492
richard <104408524+richart-coder@users.noreply.github.com>
2025-05-25 13:17:18 +0800

    test: Router(addRoute,navigate)

--- src/Router.test.js
+++ src/Router.test.js
@@ -1,1 +1,41 @@
+// @ts-nocheck
+const Router = require("./Router");
 
+function createMockWindow() {
+  const mockWindow = {
+    location: {
+      pathname: "/",
+    },
+    history: {
+      pushState: jest.fn(),
+    },
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+  };
+
+  return mockWindow;
+}
+
+describe("Router", () => {
+  let mockWindow;
+  let router;
+  beforeEach(() => {
+    mockWindow = createMockWindow();
+    router = new Router(mockWindow);
+  });
+  test("addRoute", () => {
+    router.addRoute("/", () => {});
+    expect(router.routes.size).toBe(1);
+  });
+  test("navigate", () => {
+    const handler = jest.fn();
+    router.addRoute("/test", handler);
+    router.navigate("/test");
+    expect(mockWindow.history.pushState).toHaveBeenCalledWith(
+      null,
+      "",
+      "/test"
+    );
+    expect(handler).toHaveBeenCalled();
+  });
+});
